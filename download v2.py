d2a8e080a7ff5a7ff2ee60d9bdd5fcee9fc708ca
Magnus Xu <31779143+MagnusXu@users.noreply.github.com>
2019-12-02 00:45:11 -0500

    Create download v2.py

---
+++ download v2.py
@@ -0,0 +1,17 @@
+import sys
+import csv
+from tqdm import tqdm
+from urllib.request import urlretrieve
+
+with open("dress_patterns.csv") as file:
+    reader = csv.reader(file)
+
+    header = reader.__next__()
+
+    for row in tqdm(reader):
+        uid = row[0]
+        url = row[-1]
+        try:
+            urlretrieve(url, "image/%s.png" % uid)
+        except Exception:
+            print(row, file=sys.stderr)
